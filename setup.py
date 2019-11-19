f38f7fb717beaa99fae0a34e6ea6d629fdc3022f
Leo Neto <oleoneto@gmail.com>
2019-11-19 01:57:01 -0500

    Bump to 0.0.0beta2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="eln",
-    version="0.0.0-beta.1",
+    version="0.0.0-beta.2",
     author="Leo Neto",
     author_email="apps@lehvitus.com",
     url="https://github.com/lehvitus/eln",
